a6664694491bb41ea7745f1925c7804649eb1dc8
Yuji Shimizu <yuji.shimizu@dena.com>
2014-01-11 16:36:11 -0800

    Initial Commit

---
+++ Candy Crush/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Candy Crush
+//
+//  Created by Yuji Shimizu on 1/11/14.
+//  Copyright (c) 2014 Yuji Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Candy CrushTests/Candy_CrushTests.m
@@ -0,0 +1,34 @@
+//
+//  Candy_CrushTests.m
+//  Candy CrushTests
+//
+//  Created by Yuji Shimizu on 1/11/14.
+//  Copyright (c) 2014 Yuji Shimizu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Candy_CrushTests : XCTestCase
+
+@end
+
+@implementation Candy_CrushTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
